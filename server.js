12899ea208d78ecbb7fb66f6af1c3cd178df8faa
kainvo <kainvskain@gmail.com>
2024-06-03 20:24:19 +0700

    feat: update port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.DB_URI, { dbName: 'demo_db' })
   .then(() => {
     console.log('Connected to DB');
     app.listen(process.env.PORT, () => {
-      console.log('Server is running on port 4000');
+      console.log(`Server is running on port ${process.env.PORT}`);
     })
   })
   .catch((err) => {
